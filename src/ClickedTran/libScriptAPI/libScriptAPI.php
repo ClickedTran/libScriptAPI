63d6c4e03b0793f83f399249f98bf2a69d7725a6
ClickedTran_VN <58399779+ClickedTran@users.noreply.github.com>
2025-04-11 16:40:45 +0700

    Update libScriptAPI.php

--- src/ClickedTran/libScriptAPI/libScriptAPI.php
+++ src/ClickedTran/libScriptAPI/libScriptAPI.php
@@ -3,8 +3,7 @@
 namespace ClickedTran\libScriptAPI;
 
 class libScriptAPI {
-  
-  public static function runScript(string $filePath, array $vars = []): void {
+  public static function runScript(string $filePath, array $vars = [], ?object $context = null): void {
     if (!file_exists($filePath)) {
         echo "Script file not found: $filePath\n";
         return;
@@ -12,7 +11,7 @@ class libScriptAPI {
 
     $code = file_get_contents($filePath);
     $code = preg_replace('/^\s*<\?php/', '', $code);
-    $code = preg_replace('/(\$\w+)\^([a-zA-Z_]\w*)/', '$1->$2', $code);
+    //$code = preg_replace('/(\$\w+)\^([a-zA-Z_]\w*)/', '$1->$2', $code);
     $code = preg_replace('/(\$\w+)\.([a-zA-Z_]\w*)/', '$1->$2', $code);
 
     $tmpFile = tempnam(sys_get_temp_dir(), "script_");
@@ -21,7 +20,16 @@ class libScriptAPI {
     extract($vars, EXTR_SKIP);
 
     try {
+      if ($context !== null) {
+        // Tạo anonymous function trong context object
+        $func = function() use ($tmpFile) {
+            include $tmpFile;
+        };
+        $bound = $func->bindTo($context, get_class($context));
+        $bound();
+      } else {
         include $tmpFile;
+      }
     } catch (\Throwable $e) {
         echo "[ScriptRunner Error] " . $e->getMessage() . "\n";
     }
